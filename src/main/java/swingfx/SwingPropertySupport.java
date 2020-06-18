e36150212736af029cc4977a92b388ccff2d0bb6
Eli Rosenblum <parubok4@gmail.com>
2020-06-18 10:04:30 +0300

    Updates.

--- src/main/java/swingfx/SwingPropertySupport.java
+++ src/main/java/swingfx/SwingPropertySupport.java
@@ -1,6 +1,8 @@
 package swingfx;
 
 import swingfx.beans.property.BooleanProperty;
+import swingfx.beans.property.ReadOnlyIntegerProperty;
+import swingfx.beans.property.ReadOnlyIntegerPropertyBase;
 import swingfx.beans.property.SimpleBooleanProperty;
 import swingfx.beans.property.SimpleStringProperty;
 import swingfx.beans.property.StringProperty;
@@ -9,6 +11,9 @@ import swingfx.beans.value.ChangeListener;
 import javax.swing.AbstractButton;
 import javax.swing.JComponent;
 import javax.swing.JLabel;
+import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
+import javax.swing.event.ListSelectionListener;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 import java.awt.event.ComponentListener;
@@ -26,6 +31,7 @@ public class SwingPropertySupport {
     private static final String PROP_TEXT = "swingfx-property-text";
     private static final String PROP_SELECTED = "swingfx-property-selected";
     private static final String PROP_VISIBLE = "swingfx-property-visible";
+    private static final String PROP_TABLE_SELECTED_ROW_COUNT = "swingfx-property-table-selected-row-count";
 
     // enabled:
     private static final PropertyChangeListener SWING_PROP_LISTENER_ENABLED = e -> {
@@ -159,6 +165,64 @@ public class SwingPropertySupport {
             label.putClientProperty(PROP_TEXT, p);
             label.addPropertyChangeListener("text", SWING_PROP_LISTENER_TEXT);
             p.addListener(FX_PROP_LISTENER_TEXT);
+        }
+        return p;
+    }
+
+    private static class TableSelectedRowCountProperty extends ReadOnlyIntegerPropertyBase {
+        private final JTable table;
+        private ListSelectionModel selectionModel;
+        private final ListSelectionListener selectionListener = e -> selectedRowCountChanged();
+
+        TableSelectedRowCountProperty(JTable table) {
+            this.table = Objects.requireNonNull(table);
+        }
+
+        void setSelectionModel(ListSelectionModel selectionModel) {
+            if (this.selectionModel != null) {
+                this.selectionModel.removeListSelectionListener(this.selectionListener);
+            }
+            this.selectionModel = selectionModel;
+            this.selectionModel.addListSelectionListener(this.selectionListener);
+        }
+
+        @Override
+        public int get() {
+            return table.getSelectedRowCount();
+        }
+
+        @Override
+        public JTable getBean() {
+            return table;
+        }
+
+        @Override
+        public String getName() {
+            return "selectedRowCount";
+        }
+
+        void selectedRowCountChanged() {
+            if (get() != table.getSelectedRowCount()) {
+                fireValueChangedEvent();
+            }
+        }
+    }
+
+    private static final PropertyChangeListener SELECTION_MODEL_PROPERTY_LISTENER = e -> {
+        JTable table = (JTable) e.getSource();
+        TableSelectedRowCountProperty p = (TableSelectedRowCountProperty) table.getClientProperty(PROP_TABLE_SELECTED_ROW_COUNT);
+        p.setSelectionModel(table.getSelectionModel());
+        p.selectedRowCountChanged();
+    };
+
+    public static ReadOnlyIntegerProperty selectedRowCountProperty(JTable table) {
+        Objects.requireNonNull(table, "table");
+        TableSelectedRowCountProperty p = (TableSelectedRowCountProperty) table.getClientProperty(PROP_TABLE_SELECTED_ROW_COUNT);
+        if (p == null) {
+            p = new TableSelectedRowCountProperty(table);
+            table.putClientProperty(PROP_TABLE_SELECTED_ROW_COUNT, p);
+            p.setSelectionModel(table.getSelectionModel());
+            table.addPropertyChangeListener("selectionModel", SELECTION_MODEL_PROPERTY_LISTENER);
         }
         return p;
     }
