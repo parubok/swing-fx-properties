2c4ad071e132ab8b9618ecccbb0217700913cc37
Eli Rosenblum <parubok4@gmail.com>
2020-06-19 14:47:11 +0300

    Updates.

--- src/test/java/swingfx/SwingPropertySupportTest.java
+++ src/test/java/swingfx/SwingPropertySupportTest.java
@@ -15,10 +15,15 @@ import javax.swing.DefaultListSelectionModel;
 import javax.swing.JCheckBox;
 import javax.swing.JLabel;
 import javax.swing.JTable;
+import javax.swing.JTree;
 import javax.swing.ListSelectionModel;
 import javax.swing.SwingUtilities;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableModel;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
+import javax.swing.tree.TreeModel;
+import javax.swing.tree.TreePath;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -251,6 +256,40 @@ public class SwingPropertySupportTest {
 
             ((DefaultTableModel) table.getModel()).removeRow(0);
             Assertions.assertIterableEquals(Arrays.asList(3, 2, 2, 10, 10, 9), values);
+        });
+    }
+
+    @Test
+    void tree_selectionCount_1() throws Exception {
+        SwingUtilities.invokeAndWait(() -> {
+            DefaultMutableTreeNode root = new DefaultMutableTreeNode();
+            DefaultMutableTreeNode child_1 = new DefaultMutableTreeNode();
+            DefaultMutableTreeNode child_2 = new DefaultMutableTreeNode();
+            root.add(child_1);
+            root.add(child_2);
+            TreeModel model = new DefaultTreeModel(root);
+            JTree tree = new JTree(model);
+            ReadOnlyIntegerProperty selCountProp = SwingPropertySupport.selectionCountProperty(tree);
+
+            List<Number> values = new ArrayList<>();
+            selCountProp.addListener(new ChangeListener<Number>() {
+                @Override
+                public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                    Assertions.assertTrue(SwingUtilities.isEventDispatchThread());
+                    Assertions.assertEquals(selCountProp, observable);
+                    values.add(oldValue);
+                    values.add(newValue);
+                }
+            });
+
+            Assertions.assertEquals(0, selCountProp.get());
+            tree.setSelectionPath(new TreePath(new Object[] { root, child_1 }));
+            tree.addSelectionPath(new TreePath(new Object[] { root, child_2 }));
+            Assertions.assertEquals(2, selCountProp.get());
+            Assertions.assertIterableEquals(Arrays.asList(0, 1, 1, 2), values);
+            tree.clearSelection();
+            Assertions.assertEquals(0, selCountProp.get());
+            Assertions.assertIterableEquals(Arrays.asList(0, 1, 1, 2, 2, 0), values);
         });
     }
 }
