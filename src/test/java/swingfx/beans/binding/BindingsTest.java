0289c6e5d3644360ce1895b807d1d543e07608ea
Eli Rosenblum <parubok4@gmail.com>
2020-06-22 20:19:28 +0300

    Updates.

---
+++ src/test/java/swingfx/beans/binding/BindingsTest.java
@@ -0,0 +1,27 @@
+package swingfx.beans.binding;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import swingfx.beans.property.SimpleIntegerProperty;
+import swingfx.beans.property.SimpleStringProperty;
+
+import javax.swing.SwingUtilities;
+
+class BindingsTest {
+
+    @Test
+    void createObjectBinding() throws Exception {
+        SwingUtilities.invokeAndWait(() -> {
+            SimpleStringProperty p1 = new SimpleStringProperty("abc123");
+            SimpleIntegerProperty p2 = new SimpleIntegerProperty(3);
+            ObjectBinding<String> b = Bindings.createObjectBinding(p1, p2, (v1, v2) -> v1.substring(v2.intValue()));
+            Assertions.assertEquals("123", b.get());
+
+            p2.setValue(4);
+            Assertions.assertEquals("23", b.get());
+
+            p1.setValue("HELLO!!!");
+            Assertions.assertEquals("O!!!", b.get());
+        });
+    }
+}
