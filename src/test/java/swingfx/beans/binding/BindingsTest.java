0dd7199708e7b9d16b0f88200a5cb510e95d83ea
Eli Rosenblum <parubok4@gmail.com>
2020-06-22 20:52:51 +0300

    Updates.

--- src/test/java/swingfx/beans/binding/BindingsTest.java
+++ src/test/java/swingfx/beans/binding/BindingsTest.java
@@ -5,22 +5,18 @@ import org.junit.jupiter.api.Test;
 import swingfx.beans.property.SimpleIntegerProperty;
 import swingfx.beans.property.SimpleStringProperty;
 
-import javax.swing.SwingUtilities;
-
 class BindingsTest {
     @Test
-    void createObjectBinding() throws Exception {
-        SwingUtilities.invokeAndWait(() -> {
-            SimpleStringProperty p1 = new SimpleStringProperty("abc123");
-            SimpleIntegerProperty p2 = new SimpleIntegerProperty(3);
-            ObjectBinding<String> b = Bindings.createObjectBinding(p1, p2, (v1, v2) -> v1.substring(v2.intValue()));
-            Assertions.assertEquals("123", b.get());
+    void createObjectBinding() {
+        SimpleStringProperty p1 = new SimpleStringProperty("abc123");
+        SimpleIntegerProperty p2 = new SimpleIntegerProperty(3);
+        ObjectBinding<String> b = Bindings.createObjectBinding(p1, p2, (v1, v2) -> v1.substring(v2.intValue()));
+        Assertions.assertEquals("123", b.get());
 
-            p2.setValue(4);
-            Assertions.assertEquals("23", b.get());
+        p2.setValue(4);
+        Assertions.assertEquals("23", b.get());
 
-            p1.setValue("HELLO!!!");
-            Assertions.assertEquals("O!!!", b.get());
-        });
+        p1.setValue("HELLO!!!");
+        Assertions.assertEquals("O!!!", b.get());
     }
 }
