26c06aa49ccbafff17a90f9ddb3586316c40b331
Eli Rosenblum <parubok4@gmail.com>
2020-06-24 21:53:51 +0300

    Minor changes.

--- src/main/java/org/swingfx/ComponentEnabledPropertyImpl.java
+++ src/main/java/org/swingfx/ComponentEnabledPropertyImpl.java
@@ -8,17 +8,19 @@ import javax.swing.JComponent;
 import java.beans.PropertyChangeListener;
 import java.util.Objects;
 
+import static org.swingfx.ClientProps.PROP_ENABLED;
+
 final class ComponentEnabledPropertyImpl {
-    private static final PropertyChangeListener SWING_PROP_LISTENER_ENABLED = e -> {
+    private static final PropertyChangeListener SWING_PROP_LISTENER = e -> {
         JComponent component = (JComponent) e.getSource();
-        BooleanProperty p = (BooleanProperty) component.getClientProperty(ClientProps.PROP_ENABLED);
+        BooleanProperty p = (BooleanProperty) component.getClientProperty(PROP_ENABLED);
         Boolean newValue = (Boolean) e.getNewValue();
         if (newValue.booleanValue() != p.get()) {
             p.set(newValue.booleanValue());
         }
     };
 
-    private static final ChangeListener<Boolean> FX_PROP_LISTENER_ENABLED = (observable, oldValue, newValue) -> {
+    private static final ChangeListener<Boolean> FX_PROP_LISTENER = (observable, oldValue, newValue) -> {
         BooleanProperty p = (BooleanProperty) observable;
         JComponent component = (JComponent) p.getBean();
         if (newValue.booleanValue() != component.isEnabled()) {
@@ -28,12 +30,12 @@ final class ComponentEnabledPropertyImpl {
 
     static BooleanProperty getProperty(JComponent component) {
         Objects.requireNonNull(component, "component");
-        BooleanProperty p = (BooleanProperty) component.getClientProperty(ClientProps.PROP_ENABLED);
+        BooleanProperty p = (BooleanProperty) component.getClientProperty(PROP_ENABLED);
         if (p == null) {
             p = new SimpleBooleanProperty(component, "enabled", component.isEnabled());
-            component.putClientProperty(ClientProps.PROP_ENABLED, p);
-            component.addPropertyChangeListener("enabled", SWING_PROP_LISTENER_ENABLED);
-            p.addListener(FX_PROP_LISTENER_ENABLED);
+            component.putClientProperty(PROP_ENABLED, p);
+            component.addPropertyChangeListener("enabled", SWING_PROP_LISTENER);
+            p.addListener(FX_PROP_LISTENER);
         }
         return p;
     }
