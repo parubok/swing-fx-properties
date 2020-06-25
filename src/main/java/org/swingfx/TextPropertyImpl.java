4277592f813a09e333be83c7bb11d0f56c85b77a
Eli Rosenblum <parubok4@gmail.com>
2020-06-25 14:03:15 +0300

    Minor refactoring.

--- src/main/java/org/swingfx/TextPropertyImpl.java
+++ src/main/java/org/swingfx/TextPropertyImpl.java
@@ -8,17 +8,19 @@ import javax.swing.JLabel;
 import java.beans.PropertyChangeListener;
 import java.util.Objects;
 
+import static org.swingfx.ClientProps.PROP_TEXT;
+
 final class TextPropertyImpl {
-    private static final PropertyChangeListener SWING_PROP_LISTENER_TEXT = e -> {
+    private static final PropertyChangeListener SWING_PROP_LISTENER = e -> {
         JLabel label = (JLabel) e.getSource();
-        StringProperty p = (StringProperty) label.getClientProperty(ClientProps.PROP_TEXT);
+        StringProperty p = (StringProperty) label.getClientProperty(PROP_TEXT);
         String newValue = (String) e.getNewValue();
         if (!Objects.equals(newValue, p.get())) {
             p.set(newValue);
         }
     };
 
-    private static final ChangeListener<String> FX_PROP_LISTENER_TEXT = (observable, oldValue, newValue) -> {
+    private static final ChangeListener<String> FX_PROP_LISTENER = (observable, oldValue, newValue) -> {
         StringProperty p = (StringProperty) observable;
         JLabel label = (JLabel) p.getBean();
         if (!Objects.equals(newValue, label.getText())) {
@@ -28,12 +30,12 @@ final class TextPropertyImpl {
 
     static StringProperty getProperty(JLabel label) {
         Objects.requireNonNull(label, "label");
-        StringProperty p = (StringProperty) label.getClientProperty(ClientProps.PROP_TEXT);
+        StringProperty p = (StringProperty) label.getClientProperty(PROP_TEXT);
         if (p == null) {
             p = new SimpleStringProperty(label, "text", label.getText());
-            label.putClientProperty(ClientProps.PROP_TEXT, p);
-            label.addPropertyChangeListener("text", SWING_PROP_LISTENER_TEXT);
-            p.addListener(FX_PROP_LISTENER_TEXT);
+            label.putClientProperty(PROP_TEXT, p);
+            label.addPropertyChangeListener("text", SWING_PROP_LISTENER);
+            p.addListener(FX_PROP_LISTENER);
         }
         return p;
     }
