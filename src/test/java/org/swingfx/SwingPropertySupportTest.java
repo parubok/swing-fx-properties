60ee3a86678e8a91c6fa121fe6b05acb12e7fa59
Eli Rosenblum <parubok4@gmail.com>
2020-07-21 20:32:10 +0300

    Unit test refactoring.

--- src/test/java/org/swingfx/SwingPropertySupportTest.java
+++ src/test/java/org/swingfx/SwingPropertySupportTest.java
@@ -4,66 +4,20 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import swingfx.beans.property.ObjectProperty;
 import swingfx.beans.property.ReadOnlyBooleanProperty;
-import swingfx.beans.property.ReadOnlyIntegerProperty;
-import swingfx.beans.value.ChangeListener;
-import swingfx.beans.value.ObservableValue;
 
 import javax.swing.Icon;
 import javax.swing.InputVerifier;
 import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
-import javax.swing.JTree;
 import javax.swing.SwingUtilities;
 import javax.swing.text.BadLocationException;
 import javax.swing.text.Document;
 import javax.swing.text.PlainDocument;
-import javax.swing.tree.DefaultMutableTreeNode;
-import javax.swing.tree.DefaultTreeModel;
-import javax.swing.tree.TreeModel;
-import javax.swing.tree.TreePath;
 import java.awt.Component;
 import java.awt.Graphics;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class SwingPropertySupportTest {
-
-    @Test
-    void tree_selectionCount_1() throws Exception {
-        SwingUtilities.invokeAndWait(() -> {
-            DefaultMutableTreeNode root = new DefaultMutableTreeNode();
-            DefaultMutableTreeNode child_1 = new DefaultMutableTreeNode();
-            DefaultMutableTreeNode child_2 = new DefaultMutableTreeNode();
-            root.add(child_1);
-            root.add(child_2);
-            TreeModel model = new DefaultTreeModel(root);
-            JTree tree = new JTree(model);
-            ReadOnlyIntegerProperty selCountProp = SwingPropertySupport.selectionCountProperty(tree);
-
-            List<Number> values = new ArrayList<>();
-            selCountProp.addListener(new ChangeListener<Number>() {
-                @Override
-                public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
-                    Assertions.assertTrue(SwingUtilities.isEventDispatchThread());
-                    Assertions.assertEquals(selCountProp, observable);
-                    values.add(oldValue);
-                    values.add(newValue);
-                }
-            });
-
-            Assertions.assertEquals(0, selCountProp.get());
-            tree.setSelectionPath(new TreePath(new Object[]{root, child_1}));
-            tree.addSelectionPath(new TreePath(new Object[]{root, child_2}));
-            Assertions.assertEquals(2, selCountProp.get());
-            Assertions.assertIterableEquals(Arrays.asList(0, 1, 1, 2), values);
-            tree.clearSelection();
-            Assertions.assertEquals(0, selCountProp.get());
-            Assertions.assertIterableEquals(Arrays.asList(0, 1, 1, 2, 2, 0), values);
-        });
-    }
-
     @Test
     void validInput_1() throws Exception {
         SwingUtilities.invokeAndWait(() -> {
