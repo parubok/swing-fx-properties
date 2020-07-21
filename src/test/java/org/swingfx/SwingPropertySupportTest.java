eee71456d6b03083be19a46ea767a93472cad3b3
Eli Rosenblum <parubok4@gmail.com>
2020-07-21 18:38:44 +0300

    Unit test refactoring.

--- src/test/java/org/swingfx/SwingPropertySupportTest.java
+++ src/test/java/org/swingfx/SwingPropertySupportTest.java
@@ -9,7 +9,6 @@ import swingfx.beans.value.ChangeListener;
 import swingfx.beans.value.ObservableValue;
 
 import javax.swing.BorderFactory;
-import javax.swing.DefaultListSelectionModel;
 import javax.swing.Icon;
 import javax.swing.InputVerifier;
 import javax.swing.JComponent;
@@ -18,11 +17,9 @@ import javax.swing.JPanel;
 import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.JTree;
-import javax.swing.ListSelectionModel;
 import javax.swing.SwingUtilities;
 import javax.swing.border.Border;
 import javax.swing.table.DefaultTableModel;
-import javax.swing.table.TableModel;
 import javax.swing.text.BadLocationException;
 import javax.swing.text.Document;
 import javax.swing.text.PlainDocument;
@@ -35,65 +32,9 @@ import java.awt.Component;
 import java.awt.Graphics;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class SwingPropertySupportTest {
-    @Test
-    void selectedRowCountProperty_1() throws Exception {
-        SwingUtilities.invokeAndWait(() -> {
-            TableModel model = new DefaultTableModel(10, 3);
-            JTable table = new JTable();
-            table.setModel(model);
-            table.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
-            ReadOnlyIntegerProperty selRowCountProp = SwingPropertySupport.selectedRowCountProperty(table);
-            Assertions.assertEquals(0, selRowCountProp.get());
-            table.getSelectionModel().setSelectionInterval(0, 0);
-            Assertions.assertEquals(1, selRowCountProp.get());
-
-            ListSelectionModel oldSelectionModel = table.getSelectionModel();
-
-            // test that the property continues to work after the selection model is replaced:
-            table.setSelectionModel(new DefaultListSelectionModel());
-            Assertions.assertEquals(0, selRowCountProp.get());
-            table.getSelectionModel().setSelectionInterval(0, 0);
-            Assertions.assertEquals(1, selRowCountProp.get());
-
-            table.getSelectionModel().setSelectionInterval(0, 2);
-            Assertions.assertEquals(3, selRowCountProp.get());
-            table.clearSelection();
-            Assertions.assertEquals(0, selRowCountProp.get());
-
-            oldSelectionModel.setSelectionInterval(0, 1); // should not affect the property
-            Assertions.assertEquals(0, selRowCountProp.get());
-        });
-    }
-
-    @Test
-    void selectedRowCountProperty_2() throws Exception {
-        SwingUtilities.invokeAndWait(() -> {
-            TableModel model = new DefaultTableModel(10, 3);
-            JTable table = new JTable();
-            table.setModel(model);
-            table.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
-            ReadOnlyIntegerProperty selRowCountProp = SwingPropertySupport.selectedRowCountProperty(table);
-            List<Number> values = new ArrayList<>();
-            selRowCountProp.addListener(new ChangeListener<Number>() {
-                @Override
-                public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
-                    Assertions.assertTrue(SwingUtilities.isEventDispatchThread());
-                    values.add(newValue);
-                }
-            });
-            table.getSelectionModel().setSelectionInterval(0, 1);
-            Assertions.assertEquals(2, selRowCountProp.get());
-            Assertions.assertIterableEquals(Collections.singletonList(2), values);
-            table.clearSelection();
-            Assertions.assertIterableEquals(Arrays.asList(2, 0), values);
-            table.clearSelection(); // should not fire event
-            Assertions.assertIterableEquals(Arrays.asList(2, 0), values);
-        });
-    }
 
     @Test
     void modelRowCountProperty_1() throws Exception {
